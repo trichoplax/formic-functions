5a943b8fff3f473bede9f837de6925fdbf8ea884
phagocyte <githubphagocyte@gmail.com>
2018-02-16 22:13:12 +0000

    Decrement cache count when moving from young cache to tenured

--- docs/ant-qoth.js
+++ docs/ant-qoth.js
@@ -1465,6 +1465,7 @@ function getMoveMemoiser(f) {
 				var result = cache.youngGenPerHue[hue][key]
 				if (cache.tenuredEntries < 60000) {
 					delete cache.youngGenPerHue[hue][key]
+					cache.youngEntriesPerHue[hue]--
 					cache.tenured[key] = result
 					cache.tenuredEntries++
 				}
